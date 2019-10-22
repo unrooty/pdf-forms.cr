7b65e3672d2966e056249fdee1bdb09988f4d54e
Uladzislau Volkau <uladzislau.volkau@itibo.com>
2019-10-22 16:05:08 +0300

    WIP: add code for testing

--- src/pdf-forms.cr
+++ src/pdf-forms.cr
@@ -3,16 +3,4 @@ require "./pdf_forms/field"
 
 file_path : String = "/home/volkov/Music/LANCER APPLICATION.pdf"
 
-output = Process.run(
-  "pdftk",
-  [file_path, "dump_data_fields"], output: Process::Redirect::Pipe) do |process|
-  process.output.gets_to_end
-end
-
-fields = [] of String
-
-fields = output.split("---\n").map do |field_text|
-  PdfForms::Field.new field_text if field_text =~ /FieldName:/
-end
-
-p fields.compact
+p PdfForms.new.get_field_names(file_path)
