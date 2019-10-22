a1f4f777163acb0ab7ece98534b96482cc8d4d65
Uladzislau Volkau <uladzislau.volkau@itibo.com>
2019-10-22 16:03:26 +0300

    Rework PDF class
    Add PDF fields read and initialize in Crystal using Field class

--- src/pdf_forms/pdf.cr
+++ src/pdf_forms/pdf.cr
@@ -6,15 +6,15 @@ module PdfForms
   class Pdf
     include NormalizePath
 
-    @path : String
-    @pdftk : PdftkWrapper
-    @fields : Array(Field) = [] of Field
+    setter path : String
+    setter pdftk : String
+    setter options : Hash(Symbol, String)
 
-    getter :path, :options
+    getter fields : Array(Field) | Nil = nil
 
-    def initialize(path, pdftk, options = {} of Symbol => String)
+    def initialize(path : String, pdftk : String = PDFTK_PATH, options = {} of Symbol => String)
       @options = options
-      @path = path
+      @path = normalized_path(path)
       raise "File not readable!" unless File.readable?(@path)
       @pdftk = pdftk
     end
@@ -29,15 +29,21 @@ module PdfForms
 
     # the field object for the named field
     def field(name)
-      fields.detect { |f| f.name == name }
+      fields.find { |f| f.name == name }
     end
 
     private def read_fields
-      dump_method = options[:utf8_fields] ? "dump_data_fields_utf8" : "dump_data_fields"
-      field_output = @pdftk.call_pdftk path, dump_method
-      @fields = field_output.split(/^---\n/).map do |field_text|
+      dump_method = @options[:utf8_fields]? ? "dump_data_fields_utf8" : "dump_data_fields"
+
+      @fields = fields_output(dump_method).split("---\n").map do |field_text|
         Field.new field_text if field_text =~ /FieldName:/
       end.compact
+    end
+
+    private def fields_output(dump_method)
+      Process.run(@pdftk,[@path, dump_method], output: Process::Redirect::Pipe) do |process|
+        process.output.gets_to_end
+      end
     end
   end
 end
